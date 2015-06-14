952ecdb11d72b598ceb41dd2076cecb7ab3502f5
Kevin Obermann <kevinobermann@imac.fritz.box>
2015-06-14 23:54:08 +0200

    Test

---
+++ eHaushaltsbuch/src/eHaushaltsbuch/eHaushaltsbuch.java
@@ -0,0 +1,11 @@
+package eHaushaltsbuch;
+
+public class eHaushaltsbuch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Start");
+		
+	}
+
+}
